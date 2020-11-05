3c3ab562673e1315f963b672b1015314d357ee88
JuanPablo <2986910430101@ingenieria.usac.edu.gt>
2020-11-05 15:20:12 -0600

    Se colocaron comentarios para la separacion de cada una de lasfunciones trabajadas, de igual manera se realizaron la visualizacion de perfiles

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ users=['admin']
 paswords=['admin']
 
 id2=[]
-name=[]
-Anio=[]
-Precio=[] 
-Categoria=[]
-Foto=[]
-Banner=[]
-Descripcion=[]
+name_game=[]
+anio_game=[]
+precio_game=[] 
+categoria_game=[]
+foto_game=[]
+banner_game=[]
+descripcion_game=[]
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER']="./static/archivos"
@@ -38,10 +38,13 @@ def login():
        U_login=user_login==users[posicion]
        P_login=pasword_login==paswords[posicion]
        if U_login==True and P_login==True and user_login != None and pasword_login != None:
+       	 global token
          if user_login=="admin" and pasword_login=="admin":
-          flash('Bienvenido    '+user_login) 
+          flash('Bienvenido    '+user_login)
+          token=posicion 
           return redirect(url_for('admin')) 
          else:
+          token=posicion 	
           flash('Bienvenido    '+user_login) 
           return redirect('index2')
        else:
@@ -109,22 +112,141 @@ def admin():
 def index2():
    return render_template('index2.html')    
 
-@app.route('/perfil_index2')
-def perfil_index2():
-   return render_template('perfil_index2.html')
 
+@app.route('/perfil_index2',methods=['GET','POST'])
+def perfil_index2():
+  try:
+    if request.method=="POST":
+      name=request.form.get("name")
+      surname=request.form.get("surname")
+      user=request.form.get("user")
+      pasword=request.form.get("password")
+      confirmpassword=request.form.get("confirmpassword")
+      if confirmpassword==pasword:
+              flash('Update data') 
+              names[token]=name
+              surnames[token]=surname
+              users[token]=user
+              paswords[token]=pasword
+              return redirect('index2')          
+      else:
+           user_perfil_index2=users[token]
+           surname_perfil_index2=surnames[token]
+           name_perfil_index2=names[token]
+           pasword_perfil_inedex2=paswords[token]
+           flash('Incorrect pasword confirmation')
+           return render_template('perfil_index2.html',user_index2=user_perfil_index2,surname_index2=surname_perfil_index2,
+           name_index2=name_perfil_index2,pasword_index2=pasword_perfil_inedex2)
+    else:
+      user_perfil_index2=users[token]
+      surname_perfil_index2=surnames[token]
+      name_perfil_index2=names[token]
+      pasword_perfil_inedex2=paswords[token]
+      return render_template('perfil_index2.html',user_index2=user_perfil_index2,surname_index2=surname_perfil_index2,
+      name_index2=name_perfil_index2,pasword_index2=pasword_perfil_inedex2)  
+  except:
+      return flash('User or Confirm of Password, Incorrect')
+      user_perfil_index2=users[token]
+      surname_perfil_index2=surnames[token]
+      name_perfil_index2=names[token]
+      pasword_perfil_inedex2=paswords[token]
+      return render_template('perfil_index2.html',user_index2=user_perfil_index2,surname_index2=surname_perfil_index2,
+      name_index2=name_perfil_index2,pasword_index2=pasword_perfil_inedex2) 
+
+
+@app.route('/perfil_admin',methods=['GET','POST'])
+def perfil_admin():
+  try:
+    if request.method=="POST":
+      name=request.form.get("name")
+      surname=request.form.get("surname")
+      user=request.form.get("user")
+      pasword=request.form.get("password")
+      confirmpassword=request.form.get("confirmpassword")
+      if confirmpassword==pasword:
+              flash('Update data') 
+              names[token]=name
+              surnames[token]=surname
+              users[token]=user
+              paswords[token]=pasword
+              return redirect('admin')          
+      else:
+           user_perfil_admin=users[token]
+           surname_perfil_admin=surnames[token]
+           name_perfil_admin=names[token]
+           pasword_perfil_admin=paswords[token]
+           flash('Incorrect pasword confirmation') 
+           return render_template('perfil_admin.html',user_admin=user_perfil_admin,surname_admin=surname_perfil_admin,
+           name_admin=name_perfil_admin,pasword_admin=pasword_perfil_admin)
+    else:
+      user_perfil_admin=users[token]
+      surname_perfil_admin=surnames[token]
+      name_perfil_admin=names[token]
+      pasword_perfil_admin=paswords[token]
+      return render_template('perfil_admin.html',user_admin=user_perfil_admin,surname_admin=surname_perfil_admin,
+      name_admin=name_perfil_admin,pasword_admin=pasword_perfil_admin) 	  
+  except:
+      return flash('User or Confirm of Password, Incorrect')
+      user_perfil_admin=users[token]
+      surname_perfil_admin=surnames[token]
+      name_perfil_admin=names[token]
+      pasword_perfil_admin=paswords[token]
+      return render_template('perfil_admin.html',user_admin=user_perfil_admin,surname_admin=surname_perfil_admin,
+      name_admin=name_perfil_admin,pasword_admin=pasword_perfil_admin)          
+
+@app.route('/crud')
+def crud():
+      return render_template('crud.html')
+
+#_______________________________________________crear un video juego 
+@app.route("/crear_game", methods=["GET", "POST"])
+def crear_game():
+    try:
+        if request.method == "POST":
+            name = request.form.get("name")
+            year = request.form.get("year")
+            price = request.form.get("price")
+            category = request.form.get("category")
+            url = request.form.get("url")
+            banner = request.form.get("banner")
+            description = request.form.get("description")
+            if (name != None and year != None and price != None and url != None and description != None):
+                name_game.append(name)
+                anio_game.append(year)
+                precio_game.append(price)
+                categoria_game.append(category)
+                foto_game.append(url)
+                banner_game.append(banner)
+                descripcion_game.append(description)
+                ultimo=id2[-1]
+                id2.append(ultimo+1)
+                return flash("The video game has been created")
+                return redirect("crear_game.html")
+            else:
+                return render_template("crear_game.html")
+        else:    
+             return render_template("crear_game.html")      
+    except:
+        return render_template("crear_game.html")
+
+@app.route('/ver_game')
+def ver_game():
+      return render_template('ver_game.html')     
 
 @app.route('/carga_masiva')
 def carga_masiva():
       return render_template('carga_masiva.html')    
 
-@app.route('/upload', methods=['GET','POST'])
+
+@app.route("/upload", methods=["GET", "POST"])
 def uploader():
-  if request.method=="POST":
-      f=request.files['archive']
-      filename=secure_filename(f.filename)
-      f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-      return "Archivo subido exitosamente"  
+    if request.method == "POST":
+        f = request.files["archive"]
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        return "Archivo subido exitosamente"
+
+
 
 if __name__=='__main__':
     app.run(threaded=True,debug=True,port=8000)	
